e7e08f07c9bf8f8977b2a361f96cd6656701da56
chopper6 <corbin.hopper@mail.mcgill.ca>
2020-07-15 08:59:34 +0200

    fixing sshot output

--- run.py
+++ run.py
@@ -152,7 +152,7 @@ def run_sim(params):
 		val = params['variables'][name]
 		model = model.replace("var: '" + name + "' _", "var: '" + name + "' " + str(val))
  
-	output_times = [t*params['time']/params['num_snapshots'] to t in range(len(num_snapshots))]
+	output_times = [t*params['time']/params['num_snapshots'] for t in range(len(num_snapshots))]
 	for t in output_times:
 		model += '\n%mod: alarm ' +str(t) + ' do $SNAPSHOT "abc.ka"'
 	#model = model.replace("mod: ([E] [mod] _ )=0", "mod: ([E] [mod] " + str(params['time']/params['num_snapshots']) + " )=0")
